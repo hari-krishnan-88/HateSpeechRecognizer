58a434331dd7b9b2592776d947a3982cb3a71375
hari-krishnan-88 <harikrishnankc2002@gmail.com>
2023-08-04 21:26:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,14 +4,9 @@ const nextConfig = {
     // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
     output: 'standalone', // Feel free to modify/remove this option
     
-    _experimental: {
+    // Indicate that these packages should not be bundled by webpack
+    experimental: {
         serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-    },
-    get experimental() {
-        return this._experimental;
-    },
-    set experimental(value) {
-        this._experimental = value;
     },
 };
 
